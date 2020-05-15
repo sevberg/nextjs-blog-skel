3042d7d47704794b882b2ec508298fda3394f42e
Severin <s.ryberg@fz-juelich.de>
2020-05-15 15:15:39 +0200

    bootstrapped navbar

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,15 @@
-import Navbar from "./navbar";
+import Head from "next/head";
+import Navbar from "./bootstrapped_navbar";
 
 const Layout = (props) => (
   <div>
+    <Head>
+      <title>ES-Vis</title>
+      <link
+        rel="stylesheet"
+        href="https://bootswatch.com/4/slate/bootstrap.min.css"
+      />
+    </Head>
     <Navbar />
     {props.children}
   </div>
